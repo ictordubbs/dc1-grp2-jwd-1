9cfa2ff4fabf6673ae5bc9103746c04555705c03
DUBOIS Victor <dubbs@MacBook-Pro-de-DUBOIS.local>
2018-09-26 16:40:34 +0200

    Gestion administration

---
+++ admin/layout/footer.php
@@ -0,0 +1,10 @@
+</main>
+</div>
+</div>
+
+
+
+<script src="<?php echo $siteadmin; ?>node_modules/jquery/dist/jquery.min.js"></script>
+<script src="<?php echo $siteadmin; ?>node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ admin/layout/header.php
@@ -0,0 +1,33 @@
+<?php 
+require_once __DIR__ . '/../../config/parameters.php';
+require_once __DIR__ . '/../security.php'; 
+?>
+
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Dashboard</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo $siteadmin; ?>node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="<?php echo $siteadmin; ?>css/dashboard.css" rel="stylesheet">
+    <link href="<?php echo $siteadmin; ?>node_modules/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="<?php echo $siteadmin; ?>css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php require_once __DIR__ .'/menu-top.php'; ?>
+
+    <div class="container-fluid">
+        <div class="row">
+           
+           <?php require_once __DIR__ . '/menu-left.php'; ?>
+
+            <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
